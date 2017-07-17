5dac3dc130eadd43ffd62ec527b1082d42126e58
dev1ictacademie <dev1ictacademie@gmail.com>
2017-07-17 15:44:02 +0200

    add button roll_Dices

--- src/application/controller/mainController.java
+++ src/application/controller/mainController.java
@@ -1,12 +1,48 @@
 package application.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 
 public class mainController {
-	
+	@FXML
+	private Button button_1, button_2, button_3, button_4, button_5, button_6,
+				   roll_Dices;
+
+	@FXML
+	private void rollDices_click(){
+
+	}
+
 	@FXML
 	private void dice_1_click(){
-		
+
 	}
+
+	@FXML
+	private void dice_2_click(){
+
+	}
+
+	@FXML
+	private void dice_3_click(){
+
+	}
+
+	@FXML
+	private void dice_4_click(){
+
+	}
+
+	@FXML
+	private void dice_5_click(){
+
+	}
+
+	@FXML
+	private void dice_6_click(){
+
+	}
+
 
 }
